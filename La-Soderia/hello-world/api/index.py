1a4e48ca9b80f96d319bf95f60638033939311d7
HD <harold.gomez.guzman@gmail.com>
2025-01-26 18:55:37 -0600

    Primer Commit

--- La-Soderia/hello-world/api/index.py
+++ La-Soderia/hello-world/api/index.py
@@ -1,10 +1,25 @@
-from http.server import BaseHTTPRequestHandler
+import requests
 
-class handler(BaseHTTPRequestHandler):
+def handler(event, context):
+    phone_number_id = "555428137646001"
+    message = "Hola desde Vercel!"
+    token = "EAGKP9iEo0ZAoBO2ifH4Ofjgb2ZBvCY1q8ulFETl4Ey2oU9ZA6ZCuvFcjVHSBA1casSZA4EG9VNwBgMBXKTSpZCzjkcvCcjhJ6SZBNUf6nSkKgXvGt4vwGVzaQHxiki7OTD95KOlXiQPndu2pVkltOt9s7uheWDVhg9CqEPwd760KPyzVWoSa3ZCYsxBcwoCpif2chIfaiBnqEs8oZA9yE9gqmbEkYBRAYbYfkWBzJjt8i7FMZD"
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+    url = f"https://graph.facebook.com/v16.0/{phone_number_id}/messages"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "messaging_product": "whatsapp",
+        "to": phone_number_id,
+        "text": {
+            "body": message
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return {
+        statusCode: 200,
+        body: response.text
+    }
